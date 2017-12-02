6dcd7b15fc654ca9acf063372631b44f4eac54ff
Anthony Krivonos <abkrivonos@gmail.com>
2017-12-02 12:30:38 -0500

    0.0.3 > NLP class

---
+++ algorithm/nlp.js
@@ -0,0 +1,25 @@
+// nlp.js
+
+let nlp = require('compromise');
+
+let percentNLPHit = (list, text) => {
+      // Returns null if text is null -> make sure to check for null before calling this function
+      if (!text || text == "") return null;
+      // Stores the sum of all percentages
+      let sumPct = 0;
+      // Creates an NLP object from text
+      text = nlp(text);
+      // Maps the list array into elements containing a match text and percentage match
+      list = list.map((txt) => {
+            let matched = text.match(txt);
+            let matchedText = matched.out('text'), matchedPct = matched.out('text').length / text.length || 0;
+            // Adds the percentage match between one list item and the text
+            sumPct += matchedPct;
+            return {
+                  match: matchedText,
+                  pct: matchedPct
+            };
+      });
+      // Returns the mean percentage match
+      return sumPct/list.length;
+};
