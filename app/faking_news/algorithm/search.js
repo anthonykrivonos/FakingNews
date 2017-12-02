7b6783e1a6e6605189a9994dfce7d949ce5dbf13
Anthony Krivonos <abkrivonos@gmail.com>
2017-12-02 16:27:16 -0500

    fixed async functions

--- app/faking_news/algorithm/search.js
+++ app/faking_news/algorithm/search.js
@@ -30,6 +30,7 @@ let parseResult = (title, description, link) => {
 };
 
 let parseDomain = (url) => {
+      if (!url || url == "") return "";
       var match = (url || "").match(/:\/\/(www[0-9]?\.)?(.[^/:]+)/i);
       if (match != null && match.length > 2 && typeof match[2] === 'string' && match[2].length > 0) return match[2];
       else return "";
