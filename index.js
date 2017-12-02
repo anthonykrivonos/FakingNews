e36d6642de7b93ee97db5bcf8fcf63926841c62f
Anthony Krivonos <abkrivonos@gmail.com>
2017-12-02 00:24:49 -0500

    commit

--- index.js
+++ index.js
@@ -1,19 +1,5 @@
 let Alexa = require('alexa-sdk');
 
-let handlers = {
-  'IsItTrue': () => {
-    // Where our code is
-    console.log('This ran!');
-    this.response.speak("Hello world!");
-    this.emit(':responseReady');
-  }
-};
-
-exports.handler = function (event, context, callback) {
-    const alexa = Alexa.handler(event, context, callback);
-    alexa.registerHandlers(handlers);
-    alexa.execute();
-};
 
 // Server code
 
@@ -23,7 +9,20 @@ const app = express();
 let port = process.env.PORT || 3000;
 
 app.post('/', (req, res) => {
-  res.send(`Got request: \n` + JSON.stringify(req));
+      let handlers = {
+        'IsItTrue': () => {
+          // Where our code is
+          console.log('This ran!');
+          this.response.speak("Hello world!");
+          this.emit(':responseReady');
+        }
+      };
+
+      exports.handler = function (event, context, callback) {
+          const alexa = Alexa.handler(event, context, callback);
+          alexa.registerHandlers(handlers);
+          alexa.execute();
+      };
 });
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
