b515cc6125f9dc6b59ec6b367ac50beb598d0b30
Anthony Krivonos <abkrivonos@gmail.com>
2017-12-02 11:29:35 -0500

    0.0.2 > Clickbait and search files

--- algorithm/search.js
+++ algorithm/search.js
@@ -1,9 +1,39 @@
 // search.js
+let google = require('google');
+
+let clickbait = require('./clickbait');
 
 let GOOGLE_SEARCH = process.env.GOOGLE_SEARCH;
 
-let search() => {
-
+let search = (text) => {
+      return new Promise((resolve, reject) => {
+            google.resultsPerPage = 25
+            var nextCounter = 0
+            google(text, (err, res) => {
+                  if (err) reject(err);
+                  else {
+                        let results = (res.links || []).map((lnk) => parseResult(lnk.title, lnk.description, lnk.link)).filter((res) => res.title != "");
+                        resolve(results);
+                  }
+            })
+      });
 };
 
-module.exports = search;
+let parseResult = (title, description, link) => {
+      return {
+            title,
+            description,
+            link,
+            domain: parseDomain(link)
+      }
+};
+
+let parseDomain = (url) => {
+      var match = (url || "").match(/:\/\/(www[0-9]?\.)?(.[^/:]+)/i);
+      if (match != null && match.length > 2 && typeof match[2] === 'string' && match[2].length > 0) return match[2];
+      else return null;
+};
+
+module.exports = {
+      search
+};
