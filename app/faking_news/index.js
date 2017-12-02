1f25aaae81e95b02411ae28775e131415c98240f
Anthony Krivonos <abkrivonos@gmail.com>
2017-12-02 09:56:25 -0500

    make it better

--- app/faking_news/index.js
+++ app/faking_news/index.js
@@ -6,7 +6,7 @@ var app = new alexa.app("FakingNews");
 var port = process.env.port || 3030;
 
 app.intent("IsItTrue", {
-            "slots": { "Fact": "LITERAL" },
+            "slots": { "Fact": "AMAZON.LITERAL" },
             "utterances": ["IsItTrue that {Donald Trump is orange|Fact}"]
       }, (request, response) => {
             var fact = request.slot("Fact");
@@ -14,6 +14,8 @@ app.intent("IsItTrue", {
             response.say(`The answer to ${fact} is yes.`);
       }
 );
+
+console.log(app.schemas.intent());
 
 app.express({ expressApp: express_app, checkCert: false, debug: true });
 
