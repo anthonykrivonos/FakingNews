45c15c57accfce0a62a2d911b31d4b4f02d64f34
Anthony Krivonos <abkrivonos@gmail.com>
2017-12-02 16:41:17 -0500

    fixed async functions

--- app/faking_news/algorithm/search.js
+++ app/faking_news/algorithm/search.js
@@ -7,14 +7,12 @@ let GOOGLE_SEARCH = process.env.GOOGLE_SEARCH;
 
 let search = (text) => {
       return new Promise((resolve, reject) => {
-            google.resultsPerPage = 25;
+            google.resultsPerPage = 25
             var nextCounter = 0
             if (!text || text.length == 0) reject();
             google(text, (err, res) => {
-                  console.log('Googling it');
                   if (err) reject(err);
                   else {
-                        console.log('Googled it');
                         let results = (res.links || []).filter((res) => res.title != "" && res.description != "" && res.link != "").map((lnk) => parseResult(lnk.title, lnk.description, lnk.link));
                         resolve(results);
                   }
@@ -32,16 +30,9 @@ let parseResult = (title, description, link) => {
 };
 
 let parseDomain = (url) => {
-      console.log('Parsing domain');
-      try {
-            if (!url || url == "") return "";
-            var match = (url || "").match(/:\/\/(www[0-9]?\.)?(.[^/:]+)/i);
-            if (match != null && match.length > 2 && typeof match[2] === 'string' && match[2].length > 0) return match[2];
-            else return "";
-      } catch (e) {
-            console.log('Could not parse domain');
-            return "";
-      }
+      var match = (url || "").match(/:\/\/(www[0-9]?\.)?(.[^/:]+)/i);
+      if (match != null && match.length > 2 && typeof match[2] === 'string' && match[2].length > 0) return match[2];
+      else return "";
 };
 
 module.exports = {
